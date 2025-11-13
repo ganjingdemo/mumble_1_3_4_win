b6fc0e7e2c9a6d3c23c8a973e2acd23af417341e
ganjingdemo <ganjingdemo@gmail.com>
2025-11-13 10:54:21 +0800

    [version_update] Update version from 1.3.6.41 to 1.3.6.46

--- update_version.py
+++ update_version.py
@@ -18,9 +18,9 @@ src/mumble/mumble_dll.rc
 src/murmur/murmur.plist
 src/murmur/murmur.rc'''
 
-ORIGINAL_CONTENT_LIST=["1.3.6", "1,3,6,38", "1.3.6.38"]
+ORIGINAL_CONTENT_LIST=["1.3.6", "1,3,6,41", "1.3.6.41"]
 
-UPDATE_CONTENT_LIST=["1.3.6", "1,3,6,41", "1.3.6.41"]
+UPDATE_CONTENT_LIST=["1.3.6", "1,3,6,46", "1.3.6.46"]
 
 def update_file(file_name):
 	f = open(file_name,"r")
